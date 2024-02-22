143805eaf557b1f5a3b843b6ad56a4788f5e65ec
PieterNatanael <pieternatanael@icloud.com>
2024-02-22 15:16:24 +0800

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
