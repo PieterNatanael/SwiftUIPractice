5b2e219cb5de93969f378c4eda317b7352fc2da3
PieterNatanael <pieternatanael@icloud.com>
2024-02-23 12:11:35 +0800

    Initial Commit

---
+++ SwiftUIPractice/shapesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  shapesBootcamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 23/02/24.
+//
+
+import SwiftUI
+
+struct shapesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    shapesBootcamp()
+}
