4652233922721bee08a7e710d6a00e609916dbff
PieterNatanael <pieternatanael@icloud.com>
2024-02-24 19:28:07 +0800

    Initial Commit

---
+++ SwiftUIPractice/colorBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  colorBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 24/02/24.
+//
+
+import SwiftUI
+
+struct colorBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    colorBootCamp()
+}
