b2953d1a0c1863a9da668c41627b113b3a8e132a
PieterNatanael <pieternatanael@icloud.com>
2024-02-26 13:35:47 +0800

    Initial Commit

---
+++ SwiftUIPractice/gradientsBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  gradientsBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 26/02/24.
+//
+
+import SwiftUI
+
+struct gradientsBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    gradientsBootCamp()
+}
