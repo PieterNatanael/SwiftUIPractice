645ef3b502f16ab88073c6fd67a0392ba95502c6
PieterNatanael <pieternatanael@icloud.com>
2024-02-28 11:06:41 +0800

    Initial Commit

---
+++ SwiftUIPractice/ImageBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ImageBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 28/02/24.
+//
+
+import SwiftUI
+
+struct ImageBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageBootCamp()
+}
