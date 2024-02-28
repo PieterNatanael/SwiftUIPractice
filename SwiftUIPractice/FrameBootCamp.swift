2dab602261beb55b9ebe12b10782d227b57bcd8a
PieterNatanael <pieternatanael@icloud.com>
2024-02-28 13:20:02 +0800

    Initial Commit

---
+++ SwiftUIPractice/FrameBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  FrameBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 28/02/24.
+//
+
+import SwiftUI
+
+struct FrameBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameBootCamp()
+}
