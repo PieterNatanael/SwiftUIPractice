44a51c83561dd93b1283b2eab6bd9f7fdb72db27
PieterNatanael <pieternatanael@icloud.com>
2024-02-29 18:01:43 +0800

    Initial Commit

---
+++ SwiftUIPractice/BGandOverlayBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  BGandOverlayBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 29/02/24.
+//
+
+import SwiftUI
+
+struct BGandOverlayBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BGandOverlayBootCamp()
+}
