dfed34a269d896b1672302e638afe03ff93e8916
PieterNatanael <pieternatanael@icloud.com>
2024-03-01 12:50:53 +0800

    Initial Commit

---
+++ SwiftUIPractice/StacksBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  StacksBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 01/03/24.
+//
+
+import SwiftUI
+
+struct StacksBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StacksBootCamp()
+}
