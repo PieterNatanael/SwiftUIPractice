7d0535ff7eee43f41aaee9336b8cd680455101a1
PieterNatanael <pieternatanael@icloud.com>
2024-03-02 18:57:07 +0800

    Initial Commit

---
+++ SwiftUIPractice/PaddingAndSpacerBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  PaddingAndSpacerBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 02/03/24.
+//
+
+import SwiftUI
+
+struct PaddingAndSpacerBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaddingAndSpacerBootCamp()
+}
