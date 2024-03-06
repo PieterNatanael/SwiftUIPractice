43d366d086a8fdc30bc2cbb275a895be2bf43137
PieterNatanael <pieternatanael@icloud.com>
2024-03-06 19:37:19 +0800

    Initial Commit

---
+++ SwiftUIPractice/Initializer3BootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  Initializer3BootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 06/03/24.
+//
+
+import SwiftUI
+
+struct Initializer3BootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Initializer3BootCamp()
+}
