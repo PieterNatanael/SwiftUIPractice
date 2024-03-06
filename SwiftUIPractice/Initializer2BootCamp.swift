a4805e474f456c96918c66a0376beb805e0bec15
PieterNatanael <pieternatanael@icloud.com>
2024-03-06 19:53:10 +0800

    Initial Commit

--- SwiftUIPractice/Initializer2BootCamp.swift
+++ SwiftUIPractice/Initializer2BootCamp.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct Initializer2BootCamp: View {
+    
+    let backgroundColor : Color
+    let title : String
+    let count : Int
+    
+    init(fruit : Fruit, count: Int) {
+        self.count = count
+        
+        if fruit == .Apple {
+            self.backgroundColor = .red
+        self.title = "Apple"}
+        else { self.backgroundColor = .orange
+            self.title = "Orange"
+        }
+    }
+    
+    enum Fruit {
+        case Apple
+        case Orange
+    }
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundStyle(Color.white)
+            Text(title)
+                .font(.title)
+                .foregroundStyle(Color.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(backgroundColor)
     }
 }
 
 #Preview {
-    Initializer2BootCamp()
+    HStack{
+        Initializer2BootCamp(fruit: .Apple, count: 55)
+        Initializer2BootCamp(fruit: .Orange, count: 55)
+    }
+  
 }
--- SwiftUIPractice/Initializer3BootCamp.swift
+++ SwiftUIPractice/Initializer3BootCamp.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct Initializer3BootCamp: View {
+    
+    let count : Int
+    let title : String
+    let BGColor : Color
+    let batch : String
+    
+    init(count: Int, fruit: Fruit) {
+        self.count = count
+        
+        switch fruit {
+        case .Apple, .Kiwi :
+            self.BGColor = .red
+            self.title = fruit == .Apple ? "apple" : "kiwi"
+            self.batch = fruit == .Apple ? "batch 1" : "batch3"
+            
+        case .Orange:
+            self.BGColor = .orange
+            self.title = "Orange"
+            self.batch = "Batch 2"
+            
+        case .Papaya:
+            self.BGColor = .purple
+            self.title = "Papaya"
+            self.batch = "Batch 4"
+        }
+        
+//        if fruit == .Apple {
+//            self.BGColor = .red
+//            self.title = "Apple"
+//            self.batch = "batch 1"
+//        } else {
+//            self.BGColor = .orange
+//            self.title = "Orange"
+//            self.batch = "batch2"
+//        }
+    }
+    
+    enum Fruit {
+        case Apple
+        case Orange
+        case Kiwi
+        case Papaya
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundStyle(Color.white)
+            Text("\(title)")
+                .font(.title)
+                .foregroundStyle(Color.white)
+            Text("\(batch)")
+                .font(.title)
+                .foregroundStyle(Color.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(BGColor)
     }
 }
 
 #Preview {
-    Initializer3BootCamp()
+    VStack {
+        Initializer3BootCamp(count: 55, fruit: .Apple)
+        Initializer3BootCamp(count: 55, fruit: .Orange)
+        Initializer3BootCamp(count: 55, fruit: .Kiwi)
+        Initializer3BootCamp(count: 55, fruit: .Papaya)
+    }
 }
