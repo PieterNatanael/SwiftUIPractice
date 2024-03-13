d76306a7e2698976ad14fc74bdf4d0ba910c36ce
PieterNatanael <pieternatanael@icloud.com>
2024-03-13 15:03:55 +0800

    Initial Commit

---
+++ SwiftUIPractice/ForEach2BootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ForEach2BootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 13/03/24.
+//
+
+import SwiftUI
+
+struct ForEach2BootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForEach2BootCamp()
+}
