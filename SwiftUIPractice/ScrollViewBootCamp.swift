8312b45241cf0cf26033a403658c268ec865b3c1
PieterNatanael <pieternatanael@icloud.com>
2024-03-14 12:32:32 +0800

    Initial Commit

---
+++ SwiftUIPractice/ScrollViewBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 14/03/24.
+//
+
+import SwiftUI
+
+struct ScrollViewBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewBootCamp()
+}
