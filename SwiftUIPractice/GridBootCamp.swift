450c5e5152342c7b8636b7cebf1d6473db8ab7d7
PieterNatanael <pieternatanael@icloud.com>
2024-03-15 12:07:41 +0800

    Initial Commit

---
+++ SwiftUIPractice/GridBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  GridBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 15/03/24.
+//
+
+import SwiftUI
+
+struct GridBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridBootCamp()
+}
