ec370c4a913e5a2353373173a85c87ea32b989e4
PieterNatanael <pieternatanael@icloud.com>
2024-03-16 18:01:35 +0800

    Initial Commit

---
+++ SwiftUIPractice/SafeAreaBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  SafeAreaBootCamp.swift
+//  SwiftUIPractice
+//
+//  Created by Pieter Yoshua Natanael on 16/03/24.
+//
+
+import SwiftUI
+
+struct SafeAreaBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafeAreaBootCamp()
+}
